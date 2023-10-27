6b4e13761a927fb6d135d49091faa17f95717417
bautistaarias <bautiariaaas@gmail.com>
2023-10-26 23:46:52 -0300

    Identacion del codigo

--- services/user.services.js
+++ services/user.services.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-const userSchema = require('../models/user.model');
+const User = require('../models/user.model');
 const jwt = require ('jsonwebtoken');
 
 //Registrar Usuario
@@ -8,7 +8,7 @@ const registerUserService = async ({userName, email, password, admin, suspended}
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
   // Crear usuario
-  const newUser = await userSchema.create({userName, email, password: hashedPassword, admin, suspended});
+  const newUser = await User.create({userName, email, password: hashedPassword, admin, suspended});
   // Errores de hashing o almacenamiento
   if (!newUser) throw new Error('Hubo un error al crear el nuevo usuario');
 
